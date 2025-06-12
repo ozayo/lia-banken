856d5805c548fc6a347c2c4844180d8fbd055f64
ozayo <ozay.ozdemir@gmail.com>
2025-06-12 08:56:47 +0200

    fixed errors

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,8 +1,5 @@
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 
 export default function ProtectedLayout({
@@ -13,11 +10,21 @@ export default function ProtectedLayout({
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
+        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+            <div className="flex gap-5 items-center font-semibold">
+              <Link href={"/"}>LIA Banken</Link>
+            </div>
+            <div className="flex items-center gap-2">
+              <AuthButton />
+              <ThemeSwitcher />
+            </div>
+          </div>
+        </nav>
 
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           {children}
         </div>
-
       </div>
     </main>
   );
